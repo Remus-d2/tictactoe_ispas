3c1e9a72bd22045e71fedd8232c382519279544f
Laurentiu Remus <ispas.laurentiurms@gmail.com>
2022-03-31 13:06:35 +0300

    1. Greeting Screen => main.dart
    Implementation of basic greeting screen and setup for routes

---
+++ lib/screens/gameScreen.dart
@@ -0,0 +1,13 @@
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
